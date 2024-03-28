8ae243e2f3a0f34ea317fa36fbb90b55ccb4ecf8
Bohdan <bogmira777@gmail.com>
2024-03-28 22:11:16 +0100

    First commit

---
+++ js/burger.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
+        $('.header__burger, .header__list').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+
